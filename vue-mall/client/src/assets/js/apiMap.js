b61d91e195fe9978195ea388752c735b22c59ac6
yny <1228160552@qq.com>
2019-04-26 13:45:41 +0800

    add api

--- vue-mall/client/src/assets/js/apiMap.js
+++ vue-mall/client/src/assets/js/apiMap.js
@@ -1,16 +1,19 @@
 export default {
   login: '/user/login',
   register: '/user/register',
-  addCart: '/user/addCart',
   goodsList: '/goods/list',
   priceRange: '/goods/priceRange',
+  addCart: '/cart/addCart',
   cartList: '/cart/list',
   deleteCartGoods: '/cart/delete',
   editCartGoodsNum: '/cart/editGoodsNum',
   checkOutGoods: '/cart/checkOut',
+  chooseAddress: '/orders/chooseAddress',
+  ordersInfo: '/orders/info',
   addressList: '/address/list',
   province: '/address/province',
   city: '/address/city',
   area: '/address/area',
-  addAddress: '/address/add'
+  addAddress: '/address/add',
+  deleteAddress: '/address/delete'
 }
